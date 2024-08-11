4a5a3c79d26842f56db450a8db1380ff86ecdd1c
K M Radingwana <42638046@mynwu.ac.za>
2024-08-11 23:24:20 +0200

    Initiating Security

--- Project2_API/obj/Debug/net8.0/Project2_API.AssemblyInfo.cs
+++ Project2_API/obj/Debug/net8.0/Project2_API.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Project2_API")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+ef6afbe9db09a63e9b19ce8a825572935be38443")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+c339c7f2857535b45f821403ab7d2dadc369be59")]
 [assembly: System.Reflection.AssemblyProductAttribute("Project2_API")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Project2_API")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
